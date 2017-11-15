65904bc40ce9c2d422d4f2eb5598aca540413f60
Ryan Nuzzaci <ryan.nuzzaci@machineshop.io>
2017-11-14 18:05:34 -0700

    Add mipsle support

---
+++ bolt_mipsls.go
@@ -0,0 +1,13 @@
+// +build mipsle
+
+package bolt
+
+// maxMapSize represents the largest mmap size supported by Bolt.
+const maxMapSize = 0xFFFFFFF
+
+// maxAllocSize is the size used when creating array pointers.
+const maxAllocSize = 0xFFFFFFF
+
+// Are unaligned load/stores broken on this arch?
+var brokenUnaligned = false
+
